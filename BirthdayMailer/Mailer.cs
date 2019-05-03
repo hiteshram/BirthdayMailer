720ea7f4a67ac9ea92c45af913f60341e34068a9
hiteshram.kotha@gmail.com <hiteshram.kotha@gmail.com>
2019-05-03 20:42:54 +0530

    Add project files.

---
+++ BirthdayMailer/Mailer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Mail;
+using System.Net;
+
+namespace BirthdayMailer
+{
+    class Mailer
+    {
+        
+        public void SendEmail(Dictionary<string,string> emailList)
+        {
+            
+            SmtpClient client = new SmtpClient("smtp.gmail.com");
+            client.Port = 587;
+            client.Credentials = new NetworkCredential("hiteshram.kotha@gmail.com", "Hitesh@1");
+            client.EnableSsl = true;
+            foreach (var people in emailList)
+            {
+                MailMessage mail = new MailMessage();
+                mail.From = new MailAddress("hiteshram.kotha@gmail.com");
+                mail.To.Add(people.Value);
+                mail.Subject = "Happy Birthday!!";
+                mail.Body = "Hi " + people.Key + ", Many more happy returns of the day";
+                client.Send(mail);
+            }
+        }
+    }
+}
---
+++ BirthdayMailer/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.OleDb;
+using System.Data;
+
+namespace BirthdayMailer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connString = "Provider= Microsoft.ACE.OLEDB.12.0;" + "Data Source=C:\\Users\\Hitesh\\source\\repos\\BirthdayMailer\\Sample1.xlsx" + ";Extended Properties='Excel 8.0;HDR=Yes'";
+            OleDbConnection conn = new OleDbConnection(connString);
+            conn.Open();
+            OleDbCommand cmd = new OleDbCommand("SELECT * FROM [Sheet1$]", conn);
+            OleDbDataAdapter adapter = new OleDbDataAdapter();
+            adapter.SelectCommand = cmd;
+            DataSet ds = new DataSet();
+            adapter.Fill(ds, "Birthday");
+            Dictionary<string, string> detailsList= new Dictionary<string, string>();
+            foreach (DataRowView m in ds.Tables[0].DefaultView)
+            {
+                DateTime bday = Convert.ToDateTime(m.Row.ItemArray[2].ToString());
+                if((bday.Day==DateTime.Now.Day)&&(bday.Month==DateTime.Now.Month))
+                {
+                    detailsList.Add(m.Row.ItemArray[0].ToString(), m.Row.ItemArray[1].ToString());
+                }
+            }
+            Mailer email = new Mailer();
+            email.SendEmail(detailsList);
+        }
+    }
+}
